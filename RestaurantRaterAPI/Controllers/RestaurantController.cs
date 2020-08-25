ab19b2dcf5a4b086a096962b7eb88f69656a14ce
raybrant154 <raybrant154@gmail.com>
2020-08-25 15:11:53 -0400

    Built GET methods

---
+++ RestaurantRaterAPI/Controllers/RestaurantController.cs
@@ -0,0 +1,38 @@
+﻿using RestaurantRaterAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace RestaurantRaterAPI.Controllers
+{
+    public class RestaurantController : ApiController
+    {
+                RestaurantDBContext _context = new RestaurantDBContext();
+        // -- Create (POST)
+        public async Task<IHttpActionResult> PostRestaurant(Restaurant model)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Restaurants.Add(model);
+                await _context.SaveChangesAsync();
+
+                return Ok();
+            }
+
+            return BadRequest(ModelState);
+        }
+        
+        // -- Read (GET)
+        // Get by ID
+
+        // Get All
+
+        // -- Update (PUT)
+
+        // -- Delete (DELETE)
+    }
+}
---
+++ RestaurantRaterAPI/Models/Restaurant.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRaterAPI.Models
+{
+    // Restaurant Entity (The class that gets stored in the database)
+    public class Restaurant
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public double Rating { get; set; }
+
+
+        public bool IsRecommended
+        {
+            get
+            {
+                return (Rating > 3.5);
+            }
+        }
+    }
+}
